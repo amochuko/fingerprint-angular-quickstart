f9f2223842ab53c4ae843c03b7a8918aa0f9de70
amochuko <7204868+amochuko@users.noreply.github.com>
2025-05-25 07:33:10 +0100

    update: add route component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+import { FingerprintComponent } from './fingerprint/fingerprint.component';
+import { SuccessComponent } from './success/success.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    component: FingerprintComponent,
+  },
+  {
+    path: 'success',
+    component: SuccessComponent,
+  },
+];
