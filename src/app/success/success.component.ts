32102766296111e7971a01f93ed6f5028252d23b
amochuko <7204868+amochuko@users.noreply.github.com>
2025-05-25 07:35:57 +0100

    update: add success component

---
+++ src/app/success/success.component.ts
@@ -0,0 +1,12 @@
+import { Component, Input } from '@angular/core';
+import { RouterModule } from '@angular/router';
+
+@Component({
+  selector: 'app-success',
+  imports: [RouterModule],
+  templateUrl: 'success.component.html',
+  styleUrl: 'success.component.css',
+})
+export class SuccessComponent {
+  @Input() myRequestId = ''
+}
