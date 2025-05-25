a3f3df273cb6a751bcd5ef3e5dd96379dd839104
amochuko <7204868+amochuko@users.noreply.github.com>
2025-05-25 07:35:31 +0100

    fix: update with router

--- src/app/fingerprint/fingerprint.component.ts
+++ src/app/fingerprint/fingerprint.component.ts
@@ -5,6 +5,7 @@ import {
   ReactiveFormsModule,
   Validators,
 } from '@angular/forms';
+import { Router } from '@angular/router';
 import { FingerprintjsProAngularService } from '@fingerprintjs/fingerprintjs-pro-angular';
 import { AccountService } from '../services/account.service';
 
@@ -16,6 +17,7 @@ import { AccountService } from '../services/account.service';
 })
 export class FingerprintComponent {
   isLoading = signal(false);
+  hasError = signal(false);
 
   registerForm = new FormGroup({
     username: new FormControl('', Validators.required),
@@ -24,16 +26,36 @@ export class FingerprintComponent {
 
   constructor(
     private accountService: AccountService,
-    private fingerprintService: FingerprintjsProAngularService
+    private fingerprintService: FingerprintjsProAngularService,
+    private router: Router
   ) {}
 
   async handleSubmit() {
     this.isLoading.set(true);
-    const data = await this.fingerprintService.getVisitorData();
-    
-    this.accountService.register({
-      formData: this.registerForm.value,
-      requestId: data.requestId,
-    });
+    this.hasError.set(false);
+    try {
+      const data = await this.fingerprintService.getVisitorData();
+
+      this.accountService.register({
+        formData: this.registerForm.value,
+        requestId: data.requestId,
+      });
+
+      // Redirect  on success
+      this.router.navigate([
+        '/success',
+        {
+          state: {
+            username: this.registerForm.value.username,
+            requestId: data.requestId,
+          },
+        },
+      ]);
+    } catch (err) {
+      console.error('Registration failed', err);
+      this.hasError.set(true);
+    } finally {
+      this.isLoading.set(false);
+    }
   }
 }
