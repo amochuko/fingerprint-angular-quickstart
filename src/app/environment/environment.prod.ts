498dd5c16801a7152acc497d5ddcb7708d83b660
amochuko <7204868+amochuko@users.noreply.github.com>
2025-05-25 12:29:19 +0100

    fix: add comment

--- src/app/environment/environment.prod.ts
+++ src/app/environment/environment.prod.ts
@@ -2,3 +2,5 @@ export const environment = {
   production: true,
   fingerprintApiKeyPublic: 'RtEE3PpC0Aq83yul0A6I',
 };
+
+// 1. Replace `<your-public-api-key>` with your actual public API key from the [Fingerprint dashboard](https://dashboard.fingerprint.com/api-keys).
