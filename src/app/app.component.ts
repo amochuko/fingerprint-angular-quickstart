e97497c181835b96da643fa048835a68a946fa36
amochuko <7204868+amochuko@users.noreply.github.com>
2025-05-25 03:34:55 +0100

    uppdate: FingerprintComponent

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { FingerprintComponent } from './fingerprint/fingerprint.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, ],
+  imports: [RouterOutlet, FingerprintComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = 'fingerprint angular quickstart';
