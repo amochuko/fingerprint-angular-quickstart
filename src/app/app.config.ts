5a508ea78f0a93cf94e35e0caa9e4f48fcd479b2
amochuko <7204868+amochuko@users.noreply.github.com>
2025-05-28 10:45:01 +0100

    fix: remove unused module

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,7 +1,6 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
-import { provideHttpClient, withFetch } from '@angular/common/http';
 import { routes } from './app.routes';
 import { FINGERPRINT_API_KEY_PUBLIC } from './utils/env';
 import { provideFingerprintPro } from './utils/fingerprint.provider';
@@ -10,8 +9,6 @@ export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
-    
-    provideHttpClient(withFetch()),
     provideFingerprintPro({
       loadOptions: {
         apiKey: String(FINGERPRINT_API_KEY_PUBLIC),
